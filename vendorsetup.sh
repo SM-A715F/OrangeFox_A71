1aba12f1d361e7d0de1b35e1a3a53fcb6fd5de69
geekmaster21 <maya-10@hotmail.ca>
2021-04-16 06:27:14 -0400

    first push

---
+++ vendorsetup.sh
@@ -0,0 +1,58 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+export OF_STATUS_INDENT_RIGHT=48
+export OF_STATUS_INDENT_LEFT=48
+export OF_SCREEN_H=2340
+export LC_ALL="C"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_XZ_UTILS=1
+export FOX_USE_GREP_BINARY=1
+export FOX_ENABLE_APP_MANAGER=1
+export OF_REPORT_HARMLESS_MOUNT_ISSUES=0
+export OF_NO_SPLASH_CHANGE=0
+export FOX_ADVANCED_SECURITY=1
+export OF_USE_LOCKSCREEN_BUTTON=1
+export FOX_USE_UNZIP_BINARY=1
+export FOX_USE_ZIP_BINARY=1
+export OF_USE_TWRP_SAR_DETECT=1
+export OF_USE_SYSTEM_FINGERPRINT=1
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+export OF_TARGET_DEVICES=a71,a71n
+export OF_MAINTAINER=Geekmaster21
+export FOX_VERSION=R11.1_0
+export OF_USE_HEXDUMP=1
+export FOX_DELETE_AROMAFM=1
+export TARGET_ARCH=arm64
+export FOX_REPLACE_TOOLBOX_GETPROP=0
+export FOX_USE_LZMA_COMPRESSION=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_TAR_BINARY=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_BASH_SHELL=1
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=:1
+export OF_FORCE_DISABLE_DM_VERITY_FORCED_ENCRYPTION=0
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export OF_TWRP_COMPATIBILITY_MODE=1
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export FOX_BUILD_BASH=0
+export FOX_USE_NANO_EDITOR=0
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+export FOX_REMOVE_AAPT=0
+export OF_RUN_POST_FORMAT_PROCESS=1
